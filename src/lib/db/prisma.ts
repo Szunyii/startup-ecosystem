7dac1650cf535916c831ce15dcca9d1f93582f2c
Szunyogh Dávid <szunyogh.david@niu.hu>
2025-09-01 10:42:56 +0200

    logo fix

--- src/lib/db/prisma.ts
+++ src/lib/db/prisma.ts
@@ -1,13 +1,3 @@
-// import { PrismaClient } from "@prisma/client";
-
-// const globalForPrisma = globalThis as unknown as {
-//   prisma: PrismaClient | undefined;
-// };
-// //estension miatt nem export
-// const prismaBase = globalForPrisma.prisma ?? new PrismaClient();
-
-// export const prisma = prismaBase.$extends({});
-// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prismaBase;
 
 import { PrismaClient } from "@prisma/client";
 
