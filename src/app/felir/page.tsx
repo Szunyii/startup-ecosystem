77a4c160d5a1ddd11dc907f9338a7b0a2bc703f1
Szunyogh Dávid <sz_david@icloud.com>
2026-02-19 11:48:30 +0100

    updatefelir3

--- src/app/felir/page.tsx
+++ src/app/felir/page.tsx
@@ -75,7 +75,7 @@ export default function SubscribePage() {
         }
         setStatusHtml(html);
       }
-      //   eslint-disable-next-line @typescript-eslint/no-explicit-any
+      //   eslint-disable-next-line
     } catch (err: any) {
       setStatusHtml(
         `<p style="color:red;">Hiba történt: ${err?.message ?? String(err)}</p>`,
